201928c6de69dd7400bbd76b082ff085e41c2ff9
Elen <elen.grigorian96@gmail.com>
2019-03-19 15:13:11 +0400

    added json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DataJson from './data.json'
+
 
 class App extends Component {
   render() {
+    console.log(DataJson,1111);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <h1>This is table</h1>
       </div>
     );
   }
