ad9be32a870ec83a1f9c3c715c103b6d9f897cfd
Elen <elen.grigorian96@gmail.com>
2019-03-19 17:20:10 +0400

    added atd.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-import DataJson from './data.json'
+import { Table, Tag } from 'antd';
+import dataJson from './data.json';
+const { Column} = Table;
+
 
 
 class App extends Component {
   render() {
-    console.log(DataJson,1111);
-
+    console.log(dataJson,1111);
     return (
-      <div >
-        <h1>This is table</h1>
-      </div>
+        <Table dataSource={dataJson}>
+            <Column
+                title="First Name"
+                dataIndex="firstName"
+                key="firstName"
+            />
+            <Column
+                title="Last Name"
+                dataIndex="lastName"
+                key="lastName"
+            />
+       {/*   <Column
+              title="Profession"
+              dataIndex="profession"
+              key="profession"
+              render={profession=> (
+                  <span>
+          {profession.map(prof => <Tag color="blue" key={prof}>{prof}</Tag>)}
+        </span>
+              )}
+          />*/}
+        </Table>
     );
   }
 }
