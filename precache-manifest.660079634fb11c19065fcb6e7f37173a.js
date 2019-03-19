11addaa9e041cd1eaa13c1e9b51a0733c386d0bd
Elen <elen.grigorian96@gmail.com>
2019-03-19 14:15:49 +0400

    Updates

---
+++ precache-manifest.660079634fb11c19065fcb6e7f37173a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "b7d3654034b5127a00ad",
+    "url": "/hello/static/js/main.12d04276.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/hello/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "b7d3654034b5127a00ad",
+    "url": "/hello/static/css/main.0bab9018.chunk.css"
+  },
+  {
+    "revision": "2ab061f30c9b849cd79f3c8012eb80e3",
+    "url": "/hello/index.html"
+  }
+];
