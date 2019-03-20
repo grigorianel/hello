fd7a4ee469f8bcda49ff4b04608e53d0dc2254eb
Elen <elen.grigorian96@gmail.com>
2019-03-20 11:15:12 +0400

    Fixed error for the profession

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import { Table, Tag } from 'antd';
-import dataJson from './data.json';
+import React, { Component } from "react";
+import { Table, Tag } from "antd";
+import dataJson from "./data.json";
 const { Column} = Table;
 
-
-
 class App extends Component {
-  render() {
-      const a = (profession) => console.log(typeof profession , profession) ||( profession || []).map(prof => <Tag color="blue" key={prof}>{prof}</Tag>);
-    return (
-        <Table dataSource={dataJson}>
-            <Column
-                title="First Name"
-                dataIndex="firstName"
-                key="firstName"
-            />
-            <Column
-                title="Last Name"
-                dataIndex="lastName"
-                key="lastName"
-            />
-        <Column
-              title="Profession"
-              dataIndex="profession"
-              key="profession"
-              render={profession => <span>{a(profession)}</span>}
-          />
-        </Table>
-    );
-  }
+	render() {
+		const a =(profession) => (typeof profession === "object")? profession.map(prof => <Tag color="blue" key={prof}>{prof}</Tag>): <Tag color="blue" key={profession}>{profession}</Tag>;
+		return (
+			<Table dataSource={dataJson}>
+				<Column
+					title="First Name"
+					dataIndex="firstName"
+					key="firstName"
+				/>
+				<Column
+					title="Last Name"
+					dataIndex="lastName"
+					key="lastName"
+				/>
+				<Column
+					title="Profession"
+					dataIndex="profession"
+					key="profession"
+					render={profession => <span>{a(profession)}</span>}
+				/>
+			</Table>
+		);
+	}
 }
 
 export default App;
